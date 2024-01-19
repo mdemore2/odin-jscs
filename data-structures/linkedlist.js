d4ec2034a0299625d2374e92f798be878b7c258d
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2024-01-19 17:43:25 -0500

    linkedlist impl complete

--- data-structures/linkedlist.js
+++ data-structures/linkedlist.js
@@ -1,11 +1,21 @@
 function createNode(value = null, nextNode = null) {
   return { value, nextNode };
 }
-function createLinkedList() {
+
+function createLinkedList(linkedList = []) {
   return {
     linkedList,
-    append(value) {},
-    prepend(value) {},
+    append(value) {
+      let node = createNode(value);
+      let tail = this.tail();
+      tail.nextNode = node;
+      this.inkedList.append(node);
+    },
+    prepend(value) {
+      let node = createNode(value);
+      node.nextNode = this.head();
+      this.linkedList.unshift(node);
+    },
     size() {
       return this.linkedList.length;
     },
@@ -18,9 +28,24 @@ function createLinkedList() {
     at(index) {
       return this.linkedList[index];
     },
-    pop() {},
-    contains(value) {},
-    find(value) {},
-    toString() {},
+    pop() {
+      let retVal = this.tail().value;
+      this.linkedList.pop();
+      this.tail().nextNode = null;
+      return retVal;
+    },
+    contains(value) {
+      let mappedArray = this.linkedList.map((node) => node.value);
+      return mappedArray.includes(value);
+    },
+    find(value) {
+      let mappedArray = this.linkedList.map((node) => node.value);
+      return mappedArray.indexOf(value);
+    },
+    toString() {
+      let retStr = "";
+      this.linkedList.forEach((node) => retStr.concat(`${node.value} -> `));
+      return retStr;
+    },
   };
 }
