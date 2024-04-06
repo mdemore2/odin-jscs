3fee83989ed7e2d86e6b1efb10c0d30ae9e99d92
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2024-04-05 21:16:03 -0400

    completed calculator, cipher tests and functions

--- testing/functions.js
+++ testing/functions.js
@@ -14,5 +14,45 @@ const reverse = (input_string) => {
   }
 };
 
+const calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+};
+
+const cipher = (input_string, shift) => {
+  input_string = input_string.toUpperCase();
+  var cipher_bet = ciphertext(shift);
+  var input_indices = charToNum(input_string);
+  console.log(input_indices);
+  var output_array = input_indices.map((e, i) => cipher_bet[e]);
+  console.log(output_array);
+
+  return output_array.join("");
+};
+
+const alphacode = Array.from(Array(26)).map((e, i) => i + 65);
+
+const ciphertext = (shift) => {
+  const alphabet = alphacode.map((x) => String.fromCharCode(x));
+  let ciphertext = alphabet.slice(shift).concat(alphabet.slice(0, shift));
+  return ciphertext;
+};
+
+const charToNum = (input_string) => {
+  return input_string.split("").map((e, i) => e.charCodeAt(0) - 65);
+};
+
 exports.capitalize = capitalize;
 exports.reverse = reverse;
+exports.calculator = calculator;
+exports.cipher = cipher;
--- testing/functions.test.js
+++ testing/functions.test.js
@@ -7,3 +7,23 @@ it("capitalizes", () => {
 it("reverses", () => {
   expect(functions.reverse("test")).toBe("tset");
 });
+
+it("adds", () => {
+  expect(functions.calculator.add(2, 2)).toBe(4);
+});
+
+it("subtracts", () => {
+  expect(functions.calculator.subtract(2, 2)).toBe(0);
+});
+
+it("multiplies", () => {
+  expect(functions.calculator.multiply(2, 0)).toBe(0);
+});
+
+it("divides", () => {
+  expect(functions.calculator.divide(4, 2)).toBe(2);
+});
+
+it("ciphers", () => {
+  expect(functions.cipher("TEST", 3)).toBe("WHVW");
+});
